eb8a18479bb8c7426ed66fffccb26b3cddafe7c9
David Berchelmann <david.a.berchelmann@gmail.com>
2021-04-01 16:31:27 -0500

    updated formatting and functions:

--- wrangle.py
+++ wrangle.py
@@ -14,6 +14,9 @@ warnings.filterwarnings("ignore")
 
 from sklearn.model_selection import train_test_split
 
+#Create connection~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
 # Create helper function to get the necessary connection url.
 def get_connection(db, user=user, host=host, password=password):
     '''
@@ -24,13 +27,10 @@ def get_connection(db, user=user, host=host, password=password):
 
     
     
-
+# Grab the data~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   
     
     
-
-
-# Use the above helper function and a sql query in a single function.
 def new_zillow_data():
     '''
     This function reads data from the Codeup db into a df.
@@ -72,6 +72,9 @@ def get_zillow_data(cached=False):
         df = pd.read_csv('zillowcluster_df.csv', index_col=0)
         
     return df
+
+# Clean the data~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 
 def clean_zillow(df):
     '''This functions cleans our dataset using a variety of tools:
@@ -191,3 +194,115 @@ def clean_zillow(df):
     # Set parcelid as the index
     df = df.set_index('parcelid')
     
+
+# Train/Split the data~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+
+
+
+
+
+
+
+# Wrangle the data~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+
+
+
+
+
+
+
+# Miscellaneous Prep Functions~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+''''''''''''''''''
+'                '
+'Helper Functions'
+'                '
+''''''''''''''''''
+
+
+
+def missing_zero_values_table(df):
+    
+    '''This function will look at any data set and report back on zeros and nulls for every column while also giving percentages of total values
+        and also the data types. The message prints out the shape of the data frame and also tells you how many columns have nulls '''
+    
+    
+    
+    zero_val = (df == 0.00).astype(int).sum(axis=0)
+    null_count = df.isnull().sum()
+    mis_val_percent = 100 * df.isnull().sum() / len(df)
+    mz_table = pd.concat([zero_val, null_count, mis_val_percent], axis=1)
+    mz_table = mz_table.rename(
+    columns = {0 : 'Zero Values', 1 : 'null_count', 2 : '% of Total Values'})
+    mz_table['Total Zeroes + Null Values'] = mz_table['Zero Values'] + mz_table['null_count']
+    mz_table['% Total Zero + Null Values'] = 100 * mz_table['Total Zeroes + Null Values'] / len(df)
+    mz_table['Data Type'] = df.dtypes
+    mz_table = mz_table[
+        mz_table.iloc[:,1] >= 0].sort_values(
+        '% of Total Values', ascending=False).round(1)
+    print ("Your selected dataframe has " + str(df.shape[1]) + " columns and " + str(df.shape[0]) + " Rows.\n"      
+            "There are " +  str((mz_table['null_count'] != 0).sum()) +
+          " columns that have NULL values.")
+#         mz_table.to_excel('D:/sampledata/missing_and_zero_values.xlsx', freeze_panes=(1,0), index = False)
+
+    return mz_table
+
+
+
+
+def handle_missing_values(df, prop_required_row = 0.5, prop_required_col = 0.5):
+    ''' function which takes in a dataframe, required notnull proportions of non-null rows and columns.
+    drop the columns and rows columns based on threshold:'''
+    
+    #drop columns with nulls
+    threshold = int(prop_required_col * len(df.index)) # Require that many non-NA values.
+    df.dropna(axis = 1, thresh = threshold, inplace = True)
+    
+    #drop rows with nulls
+    threshold = int(prop_required_row * len(df.columns)) # Require that many non-NA values.
+    df.dropna(axis = 0, thresh = threshold, inplace = True)
+    
+    
+    return df
+
+
+def features_missing(df):
+    
+    '''This function creates a new dataframe that analyzes the total features(columns) missing for the rows in
+    the data frame. It also give s a percentage'''
+    
+    # Locate rows with. missing features and convert into a series
+    df2 = df.isnull().sum(axis =1).value_counts().sort_index(ascending=False)
+    
+    # convert into a dataframe
+    df2 = pd.DataFrame(df2)
+    
+    # reset the index
+    df2.reset_index(level=0, inplace=True)
+    
+    # rename the columns for readability
+    df2.columns= ['total_features_missing', 'total_rows_affected'] 
+    
+    # create a column showing the percentage of features missing from a row
+    df2['pct_features_missing']= round((df2.total_features_missing /df.shape[1]) * 100, 2)
+    
+    # reorder the columns for readability/scanning
+    df2 = df2[['total_features_missing', 'pct_features_missing', 'total_rows_affected']]
+    
+    return df2
+
+def outlier_function(df, cols, k):
+	#function to detect and handle oulier using IQR rule
+    for col in df[cols]:
+        q1 = df.annual_income.quantile(0.25)
+        q3 = df.annual_income.quantile(0.75)
+        iqr = q3 - q1
+        upper_bound =  q3 + k * iqr
+        lower_bound =  q1 - k * iqr     
+        df = df[(df[col] < upper_bound) & (df[col] > lower_bound)]
+    return df
