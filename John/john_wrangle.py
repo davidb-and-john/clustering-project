3bb3f0170406398b9667dd3c11a4833b4343684f
John Grinstead <john.allen.grinstead@gmail.com>
2021-04-05 19:02:26 -0500

    worked on cluster models

--- John/john_wrangle.py
+++ John/john_wrangle.py
@@ -224,7 +224,7 @@ def clean_zillow(df):
     
      # Columns to look for outliers
     df = df[df.taxvaluedollarcnt < 1_700_000]
-    df = df[(df.calculatedfinishedsquarefeet < 5000) & (df.calculatedfinishedsquarefeet > 350)]
+    df = df[(df.calculatedfinishedsquarefeet < 3500) & (df.calculatedfinishedsquarefeet > 350)]
     df = df[(df.taxrate < 2.3) & (df.taxrate > .2)]
     df = df[df.acres < .85]
     df = df[(df.logerror < .25) & (df.logerror > -.25)]
